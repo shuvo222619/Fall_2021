e5aaaa477b7cee111b02a9b349f89c72ad8b28e1
Md Shuvo Naeem Rabbani <78009517+shuvo222619@users.noreply.github.com>
2021-10-23 16:55:32 +0600

    Add files via upload

---
+++ LabExam/employee.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2021 at 12:52 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `User Name` text NOT NULL,
+  `Password` int(11) NOT NULL,
+  `Full Name` int(11) NOT NULL,
+  `Mobile No` int(11) NOT NULL,
+  `Data of Birth` date NOT NULL,
+  `University` int(11) NOT NULL,
+  `Degree` int(11) DEFAULT NULL,
+  `Major` int(11) NOT NULL,
+  `Results` float NOT NULL,
+  `Passing Year` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
